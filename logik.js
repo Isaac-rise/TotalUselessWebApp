75bcd927677ea658f11ec408847f21db4d1bb1d3
Isaac <isaac@MacBookPro.fritz.box>
2025-08-04 23:40:43 +0200

    small text container grau leicht angepasst, dynamiken für die Speicherung der Objekte und Visualisierung im Hub und Task-Board erstellt, so das neue Objekte direkt angezeigt werden und außerdem alle nötigen Objekte beim Start geladen werden

--- logik.js
+++ logik.js
@@ -37,6 +37,8 @@ let   day_clicks            = 0;
 let   click_marker          = 0;
 let   pageInFrontHub        = 'subcontainer-hub-one';
 const homePageHub           = 'subcontainer-hub-one';
+let   objectTypes           = ['task','blog']
+let   objectColor           = ['blue','green']
 
 document.getElementById('newObjectPicker').style.display = 'none';
 document.getElementById('taskHistory').style.display = 'none';
@@ -52,11 +54,129 @@ let dbVersion;
 
 // ...und Inhalte laden
 request.onsuccess = function (event) {
-  db = event.target.result;
-  dbVersion = db.version;
+  db          = event.target.result;
+  dbVersion   = db.version;
 
   //manageTableInDb('clear','mainTable');
+  
+  // Elemente in Hub und Task laden
+  const tx    = db.transaction('mainTable','readonly');
+  const store = tx.objectStore('mainTable');
+  const index = store.index('statusTypeIndex-mainTable');
+
+
+  objectTypes.forEach(function(objectType,arrayIndex) { //für jeden Objecttyp ...
+
+    // task-board bestücken
+    const confirmedObjects = index.getAll(['confirmed',objectType]);
+    
+    confirmedObjects.onsuccess = function(event) {
+      const result = event.target.result;
+      /// wenn Tasks, dann sollen diese zunächst nach ihrer Zeit sortiert werden, damit ich diese dann in der richtigen Reihenfolge einfügen kann.
+
+      if (result.length > 0) {            // prüfen ob eine Objekt vorhanden ist 
+        
+        // Sammelcontainer clonen
+        const newContainerFTB = generateTaskBoardContainer(arrayIndex,objectType);
+        
+        // jedes gefundene Objekt aufbereiten und dann in den Sammelcontainer einfügen
+        result.forEach(function(object) { //für jedes Objekt
+
+          const title = object.Title;
+          const time  = object.Time; // time wann der Termin statt findet
+          const id    = object.id; 
+
+          console.log('title');
+          console.log(title);
+          
+          const newObject = generateSmallContainer(title,id,time);
+
+          newContainerFTB.querySelector('div').appendChild(newObject);
+      
+        });
+        document.getElementById('taskBoard').appendChild(newContainerFTB);
+      }
+
+    };
+
+    //hub bestücken
+    const waitingObjects = index.getAll(['waiting',objectType]); //...die entsprechenden Objekte raussuchen    
+
+    waitingObjects.onsuccess = function(event) {
+      const result = event.target.result;
+
+      result.forEach(function(object) {
+      
+      const title = object.Title;
+      const id    = object.id;
+
+      insertInToHub(title,id);
+      });
+
+    };
+  
+    });
+}
+
+function generateNewObject (templateName,title,id) {
+  const newObject = document.getElementById(templateName).cloneNode(true);  // titel container kopieren
+  newObject.style.removeProperty('display');                                      //denn entsprechenden container für den Titel sichbar machen
+  newObject.removeAttribute('id');
+  newObject.id = id;
+  newObject.querySelector('.title').textContent = title;
+
+  return newObject;
+}
+
+function generateTaskBoardContainer (arrayIndex,objectType){
+  const newContainerFTB = document.getElementById('template-Object-Container-for-Task-board').cloneNode(true); // oberen container kopieren
+  newContainerFTB.style.removeProperty('display');                              //den kopierten Container sichtbar machen
+  newContainerFTB.removeAttribute('id');                                        // die id des oberen container löschen
+  newContainerFTB.id = `task-board-block-${objectType}`;                                              // eigene id für container setzen
+  newContainerFTB.style.backgroundColor = objectColor[arrayIndex];
+  return newContainerFTB;
+}
+
+function generateSmallContainer (title,id,time) {
+  let newObject;
+
+  if (time) {        
+    newObject = generateNewObject('template-task-woa',title,id);      // entscheiden ob Titel mit oder ohne Add-on 
+    newObject.querySelector('.add-on').textContent = time;    
+  } else {
+    newObject = generateNewObject('template-task-wao',title,id);  
+  }
+
+  return newObject;
+}
+
+function insertInToTaskBoard (title,type,id,time) {
+  if (document.getElementById(`task-board-block-${type}`)) {
+    newObject = generateSmallContainer(title,id,time);
+    document.getElementById(`task-board-block-${type}`).querySelector('div').appendChild(newObject);
+  } else {
+    objectTypes.forEach(function(object,arrayIndex) {
+      if (object === type) {
+        newContainerFTB = generateTaskBoardContainer(arrayIndex,type);
+      }})
+    newObject = generateSmallContainer(title,id,time);
+    newContainerFTB.appendChild(newObject);
+    document.getElementById('taskBoard').appendChild(newContainerFTB);
+  }
 };
+
+function insertInToHub (title,id) {
+
+  const newObject = document.getElementById('template-unfinished-newObjects').cloneNode(true);      // template titel container kopieren
+  
+  newObject.style.removeProperty('display');                                                        // container sichtbar machen
+  newObject.removeAttribute('id');                                                                  // 
+  newObject.dbID = id;                                                                              // db-id als id setzen
+  newObject.querySelector('#title-unfinished-newObject').textContent = title;                       // title setzen
+  newObject.querySelector('#title-unfinished-newObject').removeAttribute('id');                     // id des neuen Objekt löschen 
+  document.getElementById('hub').insertBefore(newObject,document.getElementById('hub').firstChild); // in hub einfügen
+};
+
 
 //Index
 //index Daten auslesen und damit das Hub und das Task-Board bestücken
@@ -96,8 +216,15 @@ function insertInDb (dbName,object) {
     const store = db.transaction(dbName, 'readwrite').objectStore(dbName);
     /// generateShortId sollte immer einen richtigen key zurückgeben, sonst kommt es zu problemene,
     /// diese Fehler müssen abgefangen und behandelt werden
-    object.id = generateShortId();
+    const id = generateShortId();
+    object.id = id;
+    const dateObject = new Date();
+    object.storingDate = dateObject.getFullYear();
+    object.storedTime = `${dateObject.getHours()}-${dateObject.getMinutes()}-${dateObject.getSeconds()}`;
+    /// wann wurde das Element eingefügt speichern 
+
     store.put(object)
+    return id;
     };
 
 
@@ -177,7 +304,7 @@ function processingTableData (status) {
   const tableData = document.getElementById('table-newObject').querySelectorAll('.current-newObject');
   let   titleMarker = 0;
 
-  newData = {};
+  let newData = {};
 
   if (tableData.length > 0){
     tableData.forEach(row => {
@@ -185,12 +312,10 @@ function processingTableData (status) {
       let textContent   = row.querySelectorAll('td')[1].textContent.trim();
       if (typeOfContent === 'Title' && textContent.length > 0) {
         titleMarker = 1;
-        console.log('Marker');
-        console.log(titleMarker);
       }
-      if (textContent.length > 0) {
+
+      if (titleMarker === 1 && textContent.length > 0) {
         newData[typeOfContent] = textContent;
-        console.log(newData);
       };
     });
   }
@@ -199,7 +324,16 @@ function processingTableData (status) {
   newData.type        = document.getElementById('newObject-container').querySelector('h3').textContent.toLowerCase().replaceAll(" ", "-");
 
   if (titleMarker === 1) {
-    insertInDb('mainTable',newData);
+    const id = insertInDb('mainTable',newData);
+    if (status === 'waiting') {
+      insertInToHub(newData.Title,id);
+    } else {
+      if (newData.type === 'Task') {
+        insertInToTaskBoard(newData.Title,newData.type,id,newData.Time); /// hier sicher stellen  das es auch zu keinen Fehlern kommt, selbst wenn die Zeit beim erstellen nicht gesetzt wurde, Termin sollte nur bestätigt werden können wenn auch eine Zeit dabei ist. 
+      } else {
+        insertInToTaskBoard(newData.Title,newData.type,id); 
+      }
+    };
   };
 }
 
@@ -214,7 +348,7 @@ document.getElementById('newObjectPicker').addEventListener('click', function(ev
 
   // tabellen configs
   const newObjectArrays = {
-    arrayTask: ['Title'],
+    arrayTask: ['Title'],  /// das Problem mit der falschen Erkennung
     one: ['Title'],
     newObjectPicker: ['Title'],
     //arrayTask: ['Title','Content','Deadline','Categorie','Pictures','Files'],
@@ -223,20 +357,20 @@ document.getElementById('newObjectPicker').addEventListener('click', function(ev
     //arrayProject: ['Title','']
   }
 
-  document.getElementById('newObjectPicker').style.display = 'none';
+document.getElementById('newObjectPicker').style.display = 'none';
 
   // Herausfinden, welches Kind geklickt wurde
   const clickedElement = event.target;
 
   // Beispiel: data-id auslesen
   const id = clickedElement.id.split("-").at(-1); //type auslesen
-  console.log('id');
-  console.log(id);
+
   // ändert die Überschrift des newObject-previews
-  document.getElementById('heading-newObject').textContent = id
+  document.getElementById('heading-newObject').textContent = id;
 
 /// hier muss noch die Fehlerbewältigung ergäntzt werden, damit wenn ein Objekt keine Id hat etwas entsprechendes gemacht wird
   newObjectArrays[`array${id}`].forEach((item, index) => {
+
     const newLine = document.getElementById('template-row-newObject').cloneNode(true);  //clont das template
 
     newLine.querySelector('#template-row-newObject-parameter').textContent = item; //ändert den textConten in dem Id-container
@@ -255,7 +389,7 @@ document.getElementById('newObjectPicker').addEventListener('click', function(ev
 
 });
 
-document.getElementById('navigation-bar-tasks').addEventListener('click', function(event) {
+document.getElementById('navigation-bar-tasks').addEventListener('click', function(event) {      
 
   const id = event.target.id.split("-").at(1);
 
@@ -337,7 +471,7 @@ document.getElementById('navigation-bar-tasks').addEventListener('click', functi
 document.getElementById('toolBar-newObject').addEventListener('click', function(event) {
   const button = event.target.closest('button');
 
-  if (!button) return;
+  if (!button) return; /// was macht das hier ?
 
   const id = button.id.split('-').at(1);
 
@@ -445,14 +579,12 @@ window.addEventListener('touchend', (event) => {
             //Seite wird nach Links gewechselt
             if (currentPage > 0) {
                 moveToPage(currentPage - 1)
-                console.log(currentPage)
             }
 
         } else if (startPosition.touches[0].pageX > endPosition.changedTouches[0].pageX) {
             //Seite wird nach Rechts gewechselt
             if (currentPage < 3) {
                 moveToPage(currentPage + 1)
-                console.log(currentPage)
             }
         }
     }
